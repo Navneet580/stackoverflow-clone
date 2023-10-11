5e55727c8b61d7104b970fbf7016ef98dd348024
Navneet Jaiswal <navneetjaiswal69@gmail.com>
2023-10-11 23:18:55 +0530

    this is final attachment of this project

--- client/src/App.js
+++ client/src/App.js
@@ -1,14 +1,26 @@
 import { BrowserRouter as Router } from 'react-router-dom';
 
-
+import { useDispatch } from 'react-redux';
 import './App.css';
 import Navbar from './components/Navbar/Navbar';
+import AllRoutes from './AllRoutes';
+import { useEffect } from 'react';
+
+import { fetchAllQuestions } from './actions/question';
 
 function App() {
+
+  const dispatch = useDispatch()
+
+  useEffect(() => {
+    dispatch(fetchAllQuestions())
+  }, [dispatch])
+  
   return (
     <div className="App">
       <Router>
           <Navbar />
+          <AllRoutes />
       </Router>  
     </div>
   );
--- client/src/components/Navbar/Navbar.jsx
+++ client/src/components/Navbar/Navbar.jsx
@@ -1,19 +1,52 @@
-import React from 'react';
-import { Link } from 'react-router-dom';
+import React from 'react'
+import { Link } from 'react-router-dom'
+
+// import React, { useEffect } from 'react';
+// import { Link, useNavigate } from 'react-router-dom';
+// import { useSelector, useDispatch } from 'react-redux';
+// import decode from "jwt-decode";
 
 import logo from '../../assets/logo.png';
 import search from '../../assets/search-solid.svg';
 import Avatar from '../../components/Avatar/Avatar';
-import Button from '../../components/Button/Button';
-import './Navbar.css'
+// import './Navbar.css';
+// import { setCurrentUser } from '../../actions/currentUser';
+// import bars from "../../assets/bars-solid.svg";
 
-const Navbar = () => {
+// const Navbar = ({ handleSlideIn }) => {
+//     const dispatch = useDispatch();
+//     var User = useSelector((state) => state.currentUserReducer)
+//     const navigate = useNavigate();
+//     // console.log(User)
 
-    const User = null 
+//     const handleLogout = () => {
+//         dispatch({ type: "LOGOUT" });
+//         navigate("/");
+//         dispatch(setCurrentUser(null));
+//         console.log('logout')
+//       };
+    
+//       useEffect(() =>{
+//         const token = User?.token;
+//         if(token) {
+//             const decodeToken = decode(token);
+//             if(decodeToken.exp * 1000 < new Date().getTime()) {
+//                 handleLogout();
+//             }
+//         }
+//         dispatch(setCurrentUser( JSON.parse(localStorage.getItem('Profile'))));
+//       }, [User?.token, dispatch])
+    
+    const Navbar =() => {
+        var User = null
 
     return (
-        <nav>
+        <nav className='main-bar'>
             <div className="navbar">
+               {/* <button className='slide-in-icon' onClick={() => handleSlideIn()}>
+                 <img src={bars} alt='bars' width="15" />
+               </button>  */}
+
                 <Link to='/' className='nav-item nav-btn'>
                     <img src={logo} alt='logo' />
                 </Link>
@@ -24,13 +57,24 @@ const Navbar = () => {
                     <input type='text' placeholder='Search...' />
                     <img src={search} alt="search" width="18" className='search-icon' />
                 </form>
-                { User === null ?
-                    <Link to='Auth' className='nav-item nav-links'>Log in</Link> :
+                { User === null ? (
+                    <Link to='/Auth' className='nav-item nav-links'>
+                        Log in
+                    </Link> ) : (
                     <>
-                        <Link to='/' ><Avatar>R</Avatar></Link>
-                        <Button>Log out</Button>
+                        <Avatar backgroundColor='#009dff' px="10px" py="7px" borderRadius="50%" color="white">
+                           <Link 
+                             to={ `/Users/${User?.result?._id}` }
+                             style={{color:"white", textDecoration:'none'}}
+                           >
+                             {User.result.name.charAt(0).toUpperCase()}
+                           </Link>
+                        </Avatar>
+                        <button className='nav-item nav-links'>
+                            Log out
+                        </button>
                     </>
-                }
+                )}
             </div>
         </nav>
     )
